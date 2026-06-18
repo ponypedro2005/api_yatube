223fd158cd9d684e9f3fee9e4280a49e5b30a5b1
Елизавета Ганина <elizaveta.ganina22@gmail.com>
2026-06-18 23:16:40 +0300

    Fix: replace yatube_api with youtube_api in all files

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Устанавливаем настройки Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_api.settings')
 
 def pytest_configure():
     """Configure pytest environment."""
