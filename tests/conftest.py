1a75a54dfe7d02b20b486db617d48f815b68c6fb
Елизавета Ганина <elizaveta.ganina22@gmail.com>
2026-06-18 23:12:19 +0300

    Replace conftest.py with version without manage.py check

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import sys
 # Добавляем корневую папку в PYTHONPATH
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Устанавливаем настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube_api.settings')
 
 def pytest_configure():
